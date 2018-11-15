d73daa1ac1e344501df55f8640972f8e01f8d359
Dominick-Work <strictlydominick@gmail.com>
2018-11-15 10:33:24 -0500

    Added logic for opening file and checking lines for subString.

--- StringNumberIncrementer/Main.cpp
+++ StringNumberIncrementer/Main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <string>
 
+//Function Protoypes
+std::string poTopoSubstring(std::string String, std::size_t first, std::size_t last);
+
 int main()
 {
 	//--------Variable Declarations--------
@@ -9,7 +12,10 @@ int main()
 	std::string userInput;
 	std::string fullString;
 	std::string subString;
+	std::string tempString;
 	std::string fileLocation;
+	std::size_t position[4];
+	std::size_t decimalPosition;
 	bool exitFlag = true;
 
 	//Main program loop
@@ -23,16 +29,68 @@ int main()
 		if (fileLocation == "q") break;
 
 		//Request the full string in the file to be located and used for incrementing numbers
-		std::cout << "Please enter the string that in which the number contained inside will be incremented" << std::endl;
+		std::cout << "Please enter the string in which the number contained inside will be incremented" << std::endl;
 		std::cin >> fullString;
 		if (fullString == "q") break;
 
 		//Request the substring of fullString that is the number to be incremented
-		std::cout << "Enter the part of the string that contains the numbers to be incremented" << std::endl;
+		std::cout << "Enter the part of the string that contains the number to be incremented" << std::endl;
 		std::cin >> subString;
 		if (subString == "q") break;
 
-		if ()
+		//Checking for file validity
+		ReadFile.open(fileLocation);
+		if (!ReadFile.good())
+		{
+			//Request file location from user
+			std::cout << "Please enter the location of file." << std::endl;
+			std::cin >> fileLocation;
+			if (fileLocation == "q") break;
+
+			//Checking for file validity one last time
+			ReadFile.open(fileLocation);
+			if (!ReadFile.good())
+			{
+				std::cout << "Invalid File Location. Check file name or path again." << std::endl << "Press any key to exit" << std::endl;
+				std::cin;
+				break;
+			}
+		}
+
+		//File processing Loop
+		while (!ReadFile.eof())
+		{
+			std::getline(ReadFile, tempString);
+
+			//Find the full string inputed but user
+			if (tempString.find(fullString) != std::string::npos)
+			{
+				position[0] = tempString.find(fullString);
+				position[1] = position[0] + fullString.size();
+
+				tempString = poTopoSubstring(tempString, position[0], position[1]);
+
+				//Find the substring containing the numbers to increment
+				if (tempString.find(subString) != std::string::npos)
+				{
+					position[2] = tempString.find(subString);
+					position[3] = position[2] + subString.size();
+
+					tempString = poTopoSubstring(tempString, position[2], position[3]);
+
+					//Will increment starting at the last position of the subString
+
+				}
+			}
+
+		}
 	}
 
 	return 0;
+}
+
+std::string poTopoSubstring(std::string String, std::size_t first, std::size_t last)
+{
+	return String.substr(first, last - first);
+}
+
